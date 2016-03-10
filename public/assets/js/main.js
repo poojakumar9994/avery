44954781091bfd7b7fc5e1a067c847acbf6c0b0a
Pooja Kumar <poojakumar9994@gmail.com>
2016-03-10 15:22:13 +0530

    added switches

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,7 +7,16 @@ angular
 function mainController ($mdSidenav) {
   var vm = this;
 
+  vm.peopleCount = 0;
+  vm.camera = false;
+  vm.gas = false;
+  vm.motion = false;
+
   vm.toggleSidebar = function () {
     $mdSidenav('sidebar').toggle();
+  };
+
+  vm.toggleSensor = function (sensor) {
+    vm[sensor] = !vm[sensor];
   };
 }
