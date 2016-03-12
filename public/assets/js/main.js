5624bc3d4a237e5616e9f2a52ffe79517da291de
Pooja Kumar <poojakumar9994@gmail.com>
2016-03-12 14:51:43 +0530

    added joystick

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,4 @@
-/* global angular */
+/* global angular nipplejs */
 
 angular
   .module('avery', ['ngMaterial'])
@@ -11,6 +11,46 @@ function mainController ($mdSidenav) {
   vm.camera = false;
   vm.gas = false;
   vm.motion = false;
+  vm.joystick = null;
+
+  activate();
+
+  function activate () {
+    setupJoystick();
+  }
+
+  function setupJoystick () {
+    var options = {
+      zone: document.getElementById('joystick'),
+      color: '#3F51B5',
+      mode: 'static',
+      position: {
+        top: '45%',
+        left: '10%'
+      }
+    };
+
+    vm.joystick = nipplejs.create(options);
+
+    vm.joystick.on('dir', function (e, data) {
+      console.log(data.direction.angle);
+      var bg = document.getElementById('content');
+      switch (data.direction.angle) {
+        case 'up':
+          bg.style.backgroundPosition = 'top';
+          break;
+        case 'right':
+          bg.style.backgroundPosition = 'right';
+          break;
+        case 'down':
+          bg.style.backgroundPosition = 'bottom';
+          break;
+        case 'left':
+          bg.style.backgroundPosition = 'left';
+          break;
+      }
+    });
+  }
 
   vm.toggleSidebar = function () {
     $mdSidenav('sidebar').toggle();
