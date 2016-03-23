923ba58d94f6f0fec7594c040c831dd180a4baf5
rumanikumari <rumanikumari@gmail.com>
2016-03-23 17:27:54 +0530

    installed johnny-five, testing blink

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var five = require('johnny-five');
+
+var board = new five.Board();
 
 var app = express();
 var PORT = 8080;
@@ -7,6 +10,13 @@ app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
+});
+
+board.on('ready', function () {
+  app.get('/led', function (req, res) {
+    var led = new five.Led(13);
+    led.blink(500);
+  });
 });
 
 app.listen(PORT, function () {
