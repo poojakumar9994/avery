c0781c4531d7b7d542bc444760326cdf773762a9
rumanikumari <rumanikumari@gmail.com>
2016-03-23 18:00:28 +0530

    added robot router abstraction

---
+++ robot.js
@@ -0,0 +1,19 @@
+var robot = require('express').Router();
+var five = require('johnny-five');
+
+var board = new five.Board();
+
+var led;
+
+board.on('ready', function () {
+  robot.get('/led', function (req, res) {
+    led = new five.Led(13);
+    led.blink();
+  });
+});
+
+board.on('stop', function () {
+  led.off();
+});
+
+module.exports = robot;
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require('express');
-var five = require('johnny-five');
-
-var board = new five.Board();
+var robot = require('./robot.js');
 
 var app = express();
 var PORT = 8080;
@@ -12,12 +10,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-board.on('ready', function () {
-  app.get('/led', function (req, res) {
-    var led = new five.Led(13);
-    led.blink(500);
-  });
-});
+app.use('/robot', robot);
 
 app.listen(PORT, function () {
   console.log('Avery listening on port ' + PORT);
