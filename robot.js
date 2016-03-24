c81a6db117bb819007866e865058c067dbeea66c
rumanikumari <rumanikumari@gmail.com>
2016-03-24 14:29:58 +0530

    testing motor 1

--- robot.js
+++ robot.js
@@ -2,18 +2,26 @@ var robot = require('express').Router();
 var five = require('johnny-five');
 
 var board = new five.Board();
-
-var led;
+// var motorConfigs = five.Motor.SHIELD_CONFIGS.ADAFRUIT_V1;
+var led, motorRight;
 
 board.on('ready', function () {
   robot.get('/led', function (req, res) {
     led = new five.Led(13);
     led.blink();
   });
+  robot.get('/motor', function (req, res) {
+    motorRight = new five.Motor(motorConfigs.M1);
+    motorRight.start(225);
+    board.wait(1000, function () {
+      motorRight.stop();
+    });
+  });
 });
 
 board.on('exit', function () {
   led.off();
+  motorRight.stop();
 });
 
 module.exports = robot;
